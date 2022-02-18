d123fb5364a77acbe1889070b56ee14586d62cc8
Cody-Wise <cody@sharptackmedia.com>
2022-02-17 17:12:52 -0800

    Javascript Tasks

---
+++ script.js
@@ -0,0 +1,23 @@
+// let name =prompt('What is your name?', "");
+
+// alert(name);
+
+// let js = prompt('What is the "official" name of javascript?', "")
+
+// if (js=='ECMAScript'){
+//     alert('Right!');
+// }
+//     else { alert("You don't know? ECMAScript!");
+// }
+
+let favnumber = prompt("What's your favorite number?", "");
+
+if (favnumber>0) {
+    alert("Fuck Yeah");
+}
+else if (favnumber < 0){
+    alert("Too Low");
+}
+else {
+    alert(0);
+}
